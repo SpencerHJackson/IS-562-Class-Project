657ed160e84ee572d4de581278afef197bf9a617
SpencerHJackson <90439665+SpencerHJackson@users.noreply.github.com>
2022-11-07 16:51:10 -0700

    Delete useless columns

--- StudentEmployeeInformation/admin.py
+++ StudentEmployeeInformation/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Student, UniveristyClass, ISStaffMember, Season, Semester, ClassSection, ResearchSection, YearInProgram, StudentEmployeePosition
+from .models import Student, UniveristyClass, ISStaffMember, Season, Semester, YearInProgram, StudentEmployeePosition, StudentAssignment
 
 # Register your models here.
 admin.site.register(Student)
@@ -7,7 +7,6 @@ admin.site.register(UniveristyClass)
 admin.site.register(ISStaffMember)
 admin.site.register(Season)
 admin.site.register(Semester)
-admin.site.register(ClassSection)
-admin.site.register(ResearchSection)
 admin.site.register(YearInProgram)
 admin.site.register(StudentEmployeePosition)
+admin.site.register(StudentAssignment)
--- StudentEmployeeInformation/models.py
+++ StudentEmployeeInformation/models.py
@@ -47,29 +47,6 @@ class Semester(models.Model):
     def semester_name(self):
         return(str(self.season) + " " + str(self.year))
 
-class ClassSection(models.Model):
-    semester = models.ForeignKey(Semester, on_delete=models.RESTRICT, blank=True, null=True)
-    course = models.ForeignKey(UniveristyClass, on_delete=models.RESTRICT, blank=False, null=False)
-    teacher = models.ForeignKey(ISStaffMember, on_delete=models.RESTRICT, blank=True, null=True)
-
-    class Meta:
-        db_table = "class_section"
-        ordering = ['semester', 'course']
-
-    def __str__(self):
-        return(str(self.semester) + " - " + str(self.course) + " - " + str(self.teacher))
-
-class ResearchSection(models.Model):
-    semester = models.ForeignKey(Semester, on_delete=models.RESTRICT, blank=True, null=True)
-    teacher = models.ForeignKey(ISStaffMember, on_delete=models.RESTRICT, blank=True, null=True)
-
-    class Meta:
-        db_table = "research_section"
-        ordering = ['semester']
-
-    def __str__(self):
-        return(str(self.semester) + " - " + str(self.teacher))
-
 class YearInProgram(models.Model):
     student_year = models.CharField(max_length=30, blank=True, null=True)
 
@@ -111,9 +88,7 @@ class Student(models.Model):
     email = models.CharField(max_length=255, blank=True, null=True)
     phone = models.CharField(max_length=100, blank=True, null=True)
     byu_id = models.CharField(max_length=100, primary_key=True)
-    position = models.ForeignKey(StudentEmployeePosition, on_delete=models.RESTRICT, blank=True, null=True)
     employee_record = models.IntegerField(blank=True, null=True)
-    supervisor = models.ForeignKey(ISStaffMember, on_delete=models.RESTRICT, blank=True, null=True)
     hire_date = models.DateField(blank=True, null=True)
     pay_rate = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
     last_pay_increase = models.DateField(blank=True, null=True)
@@ -129,11 +104,10 @@ class Student(models.Model):
     eform_date = models.DateField(blank=True, null=True)
     workauth = models.BooleanField(blank=True, null=True)
     workauth_date = models.DateField(blank=True, null=True)
-    ta_history = models.ManyToManyField(ClassSection, blank=True)
-    ra_history = models.ManyToManyField(ResearchSection, blank=True)
     expected_hours = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
     byu_name = models.CharField(max_length=255, blank=True, null=True)
     work_assignments = models.ManyToManyField(StudentAssignment, blank=True)
+    notes = models.TextField(blank=True, null=True)
 
     class Meta:
         db_table = 'student_employee'
